6497a2cc862d9697c237e4d04b2b7e8e5e5db4ee
Yousef <yousef.abu-zidan@e-lab.com.au>
2025-07-21 13:08:10 +0000

    updates

--- scripts/post-install.sh
+++ scripts/post-install.sh
@@ -2,12 +2,18 @@
 # POST INSTALL SCRIPT
 # This script is executed after the deployment of the HPC configuration.
 
-# Add modules
+# Install amd add modules
+sudo yum install environment-modules -y
 sh /fsx/software/.modulefiles/.setup/add-to-profile.d.sh
 
 # install software
 sudo yum install -y \
     htop
+    
+# Install additional dependencies for paraview
+sudo dnf install -y xcb-util-cursor
+sudo dnf install -y libxcrypt-compat
+sudo dnf install -y libxkbcommon xcb-util xcb-util-image xcb-util-keysyms xcb-util-renderutil xcb-util-wm
 
 # # Compilation dependencies
 # # for openfoam
