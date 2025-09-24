2822f352aa29194f7676d02cfa62d8c106e4ee0c
yaz <yousef.abu-zidan@e-lab.com.au>
2025-09-24 12:39:59 +0000

    modfication

--- scripts/post-install.sh
+++ scripts/post-install.sh
@@ -8,12 +8,16 @@ sh /fsx/software/.modulefiles/.setup/add-to-profile.d.sh
 
 # install software
 sudo yum install -y \
-    htop
+    htop \
+    gnuplot
     
 # Install additional dependencies for paraview
 sudo dnf install -y xcb-util-cursor
 sudo dnf install -y libxcrypt-compat
 sudo dnf install -y libxkbcommon xcb-util xcb-util-image xcb-util-keysyms xcb-util-renderutil xcb-util-wm
+
+# Add custom settings and tools
+sudo ln -sf /fsx/.config/hpc-config/profile.d/*.sh /etc/profile.d/
 
 # # Compilation dependencies for openfoam
 # sudo dnf install clang -y
