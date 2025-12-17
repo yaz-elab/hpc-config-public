258ff2aee46e7d7dcaaaf34f1ae8c441ae45642d
yaz <yousef.abu-zidan@e-lab.com.au>
2025-12-17 21:43:32 +0000

    updates

--- scripts/post-install.sh
+++ scripts/post-install.sh
@@ -2,22 +2,23 @@
 # POST INSTALL SCRIPT
 # This script is executed after the deployment of the HPC configuration.
 
-# Install modules add link to .modulesfiles
-sudo yum install environment-modules -y
-sh /fsx/software/.modulefiles/.setup/add-to-profile.d.sh
-
 # install software
 sudo yum install -y \
+    environment-modules \
     htop \
     gnuplot
     
-# Install additional dependencies for paraview
-sudo dnf install -y xcb-util-cursor
-sudo dnf install -y libxcrypt-compat
-sudo dnf install -y libxkbcommon xcb-util xcb-util-image xcb-util-keysyms xcb-util-renderutil xcb-util-wm
+# Add link to .modulesfiles
+sh /fsx/software/.modulefiles/.setup/add-to-profile.d.sh
 
 # Add custom settings and tools
 sudo ln -sf /fsx/.config/hpc-config/profile.d/*.sh /etc/profile.d/
 
-# # Compilation dependencies for openfoam
-# sudo dnf install clang -y
+
+# # Install additional dependencies for paraview
+# sudo dnf install -y xcb-util-cursor
+# sudo dnf install -y libxcrypt-compat
+# sudo dnf install -y libxkbcommon xcb-util xcb-util-image xcb-util-keysyms xcb-util-renderutil xcb-util-wm
+
+# # # Compilation dependencies for openfoam
+# # sudo dnf install clang -y
