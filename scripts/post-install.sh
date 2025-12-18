8719278f001546fb8ebeb4524116b149b3ae479b
yaz <yousef.abu-zidan@e-lab.com.au>
2025-12-18 00:51:55 +0000

    updates

--- scripts/post-install.sh
+++ scripts/post-install.sh
@@ -14,11 +14,11 @@ sh /fsx/software/.modulefiles/.setup/add-to-profile.d.sh
 # Add custom settings and tools
 sudo ln -sf /fsx/.config/hpc-config/profile.d/*.sh /etc/profile.d/
 
-
 # # Install additional dependencies for paraview
-# sudo dnf install -y xcb-util-cursor
-# sudo dnf install -y libxcrypt-compat
-# sudo dnf install -y libxkbcommon xcb-util xcb-util-image xcb-util-keysyms xcb-util-renderutil xcb-util-wm
+sudo dnf install -y \
+     xcb-util-cursor \
+     libxcrypt-compat \
+     libxkbcommon xcb-util xcb-util-image xcb-util-keysyms xcb-util-renderutil xcb-util-wm
 
 # # # Compilation dependencies for openfoam
 # # sudo dnf install clang -y
