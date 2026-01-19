5db2a132c399834437d6c9837304f95551f8ecbb
yaz <yousef.abu-zidan@e-lab.com.au>
2026-01-19 23:15:54 +0000

    updates

--- scripts/post-install.sh
+++ scripts/post-install.sh
@@ -22,21 +22,6 @@ sh /fsx/software/.modulefiles/.setup/add-to-profile.d.sh
 # Add custom settings and tools
 sudo ln -sf /fsx/.config/hpc-config/profile.d/*.sh /etc/profile.d/
 
-# Modify teminal display style
-cat << 'EOF' >> ~/.bashrc
-
-# Custom coloured prompt
-# PS1='[${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\] \[\033[01;34m\]\w\[\033[00m\]]\$ '
-# PS1='[${debian_chroot:+($debian_chroot)}\[\033[1;37m\]\u@\h\[\033[00m\] \[\033[1;90m\]\w\[\033[00m\]]\$ '
-PS1='$( 
-    if [[ -n "$SLURM_JOB_END_TIME" && -n "$SLURM_JOB_START_TIME" ]]; then
-        t=$(( (SLURM_JOB_END_TIME - $(date +%s) + 30)/60 ))
-        printf "(%dm) " "$t"
-    fi
-)[${debian_chroot:+($debian_chroot)}\[\033[1;37m\]\u@\h\[\033[00m\] \[\033[1;90m\]\w\[\033[00m\]]\$ '
-
-EOF
-
 # # Compilation dependencies for openfoam
 # sudo dnf install clang -y
 
