b102f5024c349a554f9ff7349867ab2843d906c6
yaz <yousef.abu-zidan@e-lab.com.au>
2026-01-20 10:02:14 +0000

    updates

--- scripts/post-install.sh
+++ scripts/post-install.sh
@@ -22,6 +22,18 @@ sh /fsx/software/.modulefiles/.setup/add-to-profile.d.sh
 # Add custom settings and tools
 sudo ln -sf /fsx/.config/hpc-config/profile.d/*.sh /etc/profile.d/
 
-# # Compilation dependencies for openfoam
-# sudo dnf install clang -y
+# Setup cluster users
+sudo sh /fsx/.config/hpc-config/users/user-setup.sh
 
+
+# Append to bashrc
+sudo tee -a /home/ec2-user/.bashrc >/dev/null <<'EOF'
+
+slurm_timer() {
+    if [[ -n "$SLURM_JOB_END_TIME" && -n "$SLURM_JOB_START_TIME" ]]; then
+        local t=$(( (SLURM_JOB_END_TIME - $(date +%s) + 30)/60 ))
+        printf "(%dm) " "$t"
+    fi
+}
+export PS1='$(slurm_timer)[${debian_chroot:+($debian_chroot)}\[\033[1;37m\]\u@\h\[\033[00m\] \[\033[1;90m\]\w\[\033[00m\]]\$ '
+EOF
